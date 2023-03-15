72c15fa132d9f8f6cdc17351e8ed1c5e8f054994
FayasTM <112546644+FayasTM@users.noreply.github.com>
2023-03-15 14:10:58 +0530

    Lectures update

--- src/com/ictech/code/Lecturer.java
+++ src/com/ictech/code/Lecturer.java
@@ -1,6 +1,11 @@
 package com.ictech.code;
 
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
 
 public class Lecturer extends JFrame{
     private JPanel PanelLecturer;
@@ -17,4 +22,67 @@ public class Lecturer extends JFrame{
     private JPanel tbbAttendace;
     private JPanel tbbMedcal;
     private JLabel lblgrp7;
+    private JTable tablee1;
+    private JButton stud1;
+
+    Connection con;
+    PreparedStatement pst;
+
+    public Lecturer() {
+        connect();
+        table_load();
+        stud1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public void table_load(){
+        try{
+        pst = con.prepareStatement("SELECT * FROM student");
+        ResultSet rs = pst.executeQuery();
+        tablee1.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame lecture = new JFrame("PanelLecturer");
+        lecture.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        lecture.pack();
+        lecture.setContentPane(new Lecturer().PanelLecturer);
+        lecture.setVisible(true);
+    }
+
+    public void connect(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms-java","root","1234");
+            System.out.println("DB Connected");
+        }
+
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        catch (SQLException e){
+            e.printStackTrace();
+
+        }
 }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
+
+
+
+
+
